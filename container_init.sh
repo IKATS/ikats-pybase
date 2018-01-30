13de0481db81b9452631063f6881d232c96710cd
ftortora <fabien.tortora@c-s.fr>
2018-01-30 15:44:06 +0100

    [#169175] updating containter_init with gunicorn environment
    Signed-off-by: ftortora <fabien.tortora@c-s.fr>

--- container_init.sh
+++ container_init.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 
-set -xe
+root_path=$(pwd)"/"
+# SPARK_HOME environment variable
+spark_home=/opt/spark-1.6.1
+# Specify the build path
+build_path=/build
 
 function test_for_variable {
   vname=$1
@@ -37,19 +42,8 @@ do
   test_for_variable $v
 done
 
-#!/bin/bash
-
-root_path=$(pwd)"/"
-# SPARK_HOME environment variable
-spark_home=/opt/spark-1.6.1
-# Specify the build path
-build_path=/build/
-
-# Managing path
-settings="settings.environ"
-
 # Build path
-echo -e "\nGenerating path"
+echo -e "Generating path"
 mkdir -p ${build_path} || exit 1;
 mkdir -p /logs || exit 1;
 
@@ -57,25 +51,21 @@ mkdir -p /logs || exit 1;
 sources_path=${root_path}_sources/
 
 # Building deploy src structure
-echo -e "\nBuilding deployed structure"
-cd ${build_path}
+echo -e "Building deployed structure"
 cp -rf ${sources_path}ikats_core/src/* ${build_path} || exit 1;
 cp -rf ${sources_path}ikats_algos/src/* ${build_path} || exit 1;
 cp -rf ${sources_path}ikats_django/src/* ${build_path} || exit 1;
 
 cp ${root_path}gunicorn.py.ini ${build_path} || exit 1;
 
-cd ${build_path}
-
-echo -e "\nConfiguring python"
-
 # Overriding ikats config
 echo "Configuring the node"
-config_file=${build_path}ikats/core/config/ikats.conf
+cd ${build_path}
+config_file=${build_path}/ikats/core/config/ikats.conf
 sed -i -e "s/opentsdb\.read\.ip.*$/opentsdb.read.ip = ${OPENTSDB_READ_HOST}/" ${config_file}
 sed -i -e "s/opentsdb\.read\.port.*$/opentsdb.read.port = ${OPENTSDB_READ_PORT}/" ${config_file}
 sed -i -e "s/opentsdb\.write\.ip.*$/opentsdb.write.ip = ${OPENTSDB_WRITE_HOST}/" ${config_file}
-sed -i -e "s/opentsdb\.write\.port.*$/opentsdb.write.port = ${OPENTSDB_WRITE_PORT}/" ${config_file}
+sed -i -e "s/opentsdb\.write\.port.*$/opentsdb.write.port = ${OPENTSDB_WRITE_PORTOPENTSDB_WRITE_PORT}/" ${config_file}
 sed -i -e "s/tdm\.ip.*$/tdm.ip = ${TDM_HOST}/" ${config_file}
 sed -i -e "s/tdm\.port.*$/tdm.port = ${TDM_PORT}/" ${config_file}
 sed -i    "s/spark.url=.*/spark.url=$SPARK_MASTER/g" ${config_file}
@@ -87,19 +77,24 @@ then
    PATH=$PATH:/usr/local/bin/
 fi
 
-# Handling SPARK needs
-export SPARK_HOME=${spark_home}
-export PYSPARK_PYTHON=${build_path}bin/python
-echo -e "SPARK_HOME set to ${SPARK_HOME}"
-export PYTHONPATH=${build_path}ikats/processing
-
-cd ${build_path}ikats/processing
+sleep 15
+cd ${build_path}/ikats/processing
+python3 manage.py migrate --settings=ikats_processing.settings.docker
 
 # Starting new Gunicorn
-echo -e "\nStarting Gunicorn"
-export DJANGO_SETTINGS_MODULE="ikats_processing.settings.docker"
+echo -e "Starting Gunicorn"
 
-echo "gunicorn -c /build/gunicorn.py.ini --bind 0.0.0.0:8000 ikats_processing.wsgi:application"
-sleep infinity
-# gunicorn launched foreground (daemon false)
-gunicorn -c ${build_path}gunicorn.py.ini --bind 0.0.0.0:8000 ikats_processing.wsgi:application
+# Gunicorn launched foreground (daemon false)
+gunicorn \
+    -c ${build_path}/gunicorn.py.ini \
+    --pythonpath "${build_path}" \
+    --pythonpath "${build_path}/processing" \
+    --pythonpath "${build_path}" \
+    --pythonpath "${build_path}/algo/contrib" \
+    --pythonpath "${build_path}" \
+    --error-logfile - \
+    --env SPARK_HOME=${spark_home} \
+    --env PYSPARK_PYTHON=python3 \
+    --env DJANGO_SETTINGS_MODULE=ikats_processing.settings.docker \
+    --chdir ${build_path}/ikats/processing \
+    --bind 0.0.0.0:8000 ikats_processing.wsgi || echo "Failed!"
