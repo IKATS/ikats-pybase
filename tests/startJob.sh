929b0532b1821251b669d4d524b413f0c717dc4a
ftortora <fabien.tortora@c-s.fr>
2018-03-01 09:47:53 +0100

    [#170555] docker_bindings moved to temporary folder
    Signed-off-by: ftortora <fabien.tortora@c-s.fr>

--- tests/startJob.sh
+++ tests/startJob.sh
@@ -9,18 +9,18 @@ trap "docker-compose down 2>/dev/null; exit 1" INT KILL QUIT
 # Prepare docker_bindings
 echo "Getting fresh docker bindings"
 # Get new data
-export pathToDockerBindings=$(pwd)
+export pathToDockerBindings=$(mktemp -d)
 # Data are stored in /opt on remote jenkins slave host because of forbidden access to the NAS
-pathToDockerBindingsRepo=/opt/
+pathToDockerBindingsRepo=${1:-/IKATSDATA/docker_bindings/}
 
 if [[ ! -d ${pathToDockerBindingsRepo} ]]
 then
-  echo "NAS not reachable. Can't get docker_bindings"
+  echo "Can't get docker_bindings. Unreachable folder"
   exit 2;
 fi
 
 dockerBindingsName="docker_bindings_EDF_portfolio"
-cp ${pathToDockerBindingsRepo}/${dockerBindingsName}.tar.gz ${pathToDockerBindings} &
+cp ${pathToDockerBindingsRepo}${dockerBindingsName}.tar.gz ${pathToDockerBindings} &
 # Clean previous one if exists
 rm -rf ${pathToDockerBindings}/docker_bindings 2 > /dev/null &
 # Previous actions were performed in parallel, wait until all previous actions are completed
@@ -47,9 +47,12 @@ docker cp assets/testRunner.sh ${containerName}:${IKATS_PATH}/
 
 # Execute the test campaign inside the docker container
 docker exec -it --user root ${containerName} bash ${IKATS_PATH}/testPrepare.sh
-sleep 15
+sleep 30
 docker exec -it --user ikats ${containerName} bash ${IKATS_PATH}/testRunner.sh
 EXIT_STATUS=$?
+
+# Test purposes
+docker ps
 
 # Get the results from docker container to host
 docker cp ${containerName}:${IKATS_PATH}/ikats/processing/reports/junit.xml ./junit.xml
