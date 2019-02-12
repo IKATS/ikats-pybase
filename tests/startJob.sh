f119198f9e7f7e22e62ff3d5452e601acb6bd7ac
ftorto <dev.torto@gmail.com>
2019-02-12 13:49:47 +0100

    updated path to docker_bindings location for Unit tests in CI
    art #3810
    Changelog: (FIX) updated Unit Test dependency location (#3810)

--- tests/startJob.sh
+++ tests/startJob.sh
@@ -10,7 +10,7 @@ trap "echo 'Stopping...';docker-compose down >/dev/null 2>&1; exit 1" INT KILL Q
 dockerBindingsName="docker_bindings_EDF_portfolio"
 echo "Getting fresh docker bindings"
 # Get new data
-pathToDockerBindingsRepo=${1:-/IKATSDATA/docker_bindings/}
+pathToDockerBindingsRepo=${1:-/mnt/IKATSNAS/docker_bindings/}
 
 if [[ ! -d ${pathToDockerBindingsRepo} ]]
 then
